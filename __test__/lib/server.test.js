dcb50139938127fd8de56b9f9ac4ded3ded099f0
mai <mai.yusuf28@yahoo.com>
2020-01-29 23:53:29 +0200

    write the test to products and categories route are done

--- __test__/lib/server.test.js
+++ __test__/lib/server.test.js
@@ -21,3 +21,78 @@ it ('responsed with 500 when we do not know what you talk about or our server no
       expect(results.status).toBe(500);
     });
 });
+
+
+
+
+describe('api server testing the producte routes' ,()=>{
+  it ('responsed with 200 status with /api/v1/product  get request',() =>{
+    return mockRequest
+    .get('api/va/products')
+    .then(results =>{
+      expect(results.status).toBe(200);
+      expect(typeof results.body.results).toBe('object')
+      console.log("mmmmmm" , results.body);
+      console.log('2222222' , results.body.results);
+    });
+  });
+
+  it ('responsed propaply( defiend , status 201) to post request at /api/v1/products', ()=>{
+    return mockRequest 
+    .post('api/v1/products')
+    .send({ category :'shoes'})
+    .then(results =>{
+      expect (results.body).toBeDefiend();
+      epect(results.status).toBe(201);
+      expect(results.body.category).toEqual('shoes')
+    })
+  })
+  it('reponsed propaply with 200 atatus to delete request at api/v1/products ',() =>{
+    return mockRequest
+    .post('api/v1/products')
+    .send({ category :'shoes'})
+    .delete('api/v1/product/:1')
+    .then(results=>{
+      expect(results.status).toBe(200)
+      expect(results.body).toEqual('item deleted ')
+    })
+
+  })
+})
+
+
+
+describe('api server testing the categories routes' ,()=>{
+  it ('responsed with 200 status with /api/v1/categories  get request',() =>{
+    return mockRequest
+    .get('api/va/categories')
+    .then(results =>{
+      expect(results.status).toBe(200);
+      expect(typeof results.body.results).toBe('object')
+      console.log("mmmmmm" , results.body);
+      console.log('2222222' , results.body.results);
+    });
+  });
+
+  it ('responsed propaply( defiend , status 201) to post request at /api/v1/categories', ()=>{
+    return mockRequest 
+    .post('api/v1/categories')
+    .send({ category :'shoes'})
+    .then(results =>{
+      expect (results.body).toBeDefiend();
+      epect(results.status).toBe(201);
+      expect(results.body.category).toEqual('shoes')
+    })
+  })
+  it('reponsed propaply with 200 atatus to delete request at api/v1/categories ',() =>{
+    return mockRequest
+    .post('api/v1/categories')
+    .send({ category :'shoes'})
+    .delete('api/v1/categories/:1')
+    .then(results=>{
+      expect(results.status).toBe(200)
+      expect(results.body).toEqual('item deleted ')
+    })
+
+  })
+})
