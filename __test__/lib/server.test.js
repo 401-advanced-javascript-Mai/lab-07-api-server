79295a859702c8fbb4148b1df994c655633f60a2
mai <mai.yusuf28@yahoo.com>
2020-01-29 20:12:54 +0200

    write the function of error

--- __test__/lib/server.test.js
+++ __test__/lib/server.test.js
@@ -1,23 +1,23 @@
 'use strict' ;
 
-const {server} = require('../../lib/server.js')
+const {server} = require('../../lib/server.js');
 const superTest = require('supertest');
 const mockRequest = superTest(server) ;
 
 describe('api server testing error in this  ', ()=>{
-    it('responed with Not found error (404) if the route is incorrect ' ,()=>{
-        return mockRequest 
-        .get('/incorrect-route')
-        .then(results =>{
-            expect(results.status).toBe(401);
-        })
-        .catch(console.error);
+  it('responed with Not found error (404) if the route is incorrect ' ,()=>{
+    return mockRequest 
+      .get('/incorrect-route')
+      .then(results =>{
+        expect(results.status).toBe(401);
+      })
+      .catch(console.error);
+  });
+});
+it ('responsed with 500 when we do not know what you talk about or our server not accept the modification (example when i throw my error in specific route ',() =>{
+  return mockRequest
+    .get('/I-throw-error')
+    .then(results =>{
+      expect(results.status).toBe(500);
     });
 });
-    it ('responsed with 500 when we do not know what you talk about or our server not accept the modification (example when i throw my error in specific route ',() =>{
-        return mockRequest
-        .get('/I-throw-error')
-        .then(results =>{
-            expect(results.status).toBe(500)
-        })
-    })
